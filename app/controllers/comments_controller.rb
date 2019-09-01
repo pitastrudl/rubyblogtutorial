e9fd154c88bbf210d464dd62b2940572b95fd5b4
pitastrudl <pitastrudl@gmail.com>
2019-09-01 10:22:20 +0200

    added comments, added deletion

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create 
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comments_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private 
+    def comments_params 
+      params.require(:comment).permit(:commenter,:body)
+    end
+end
